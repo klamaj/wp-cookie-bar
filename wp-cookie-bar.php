5b506c32a8191ccde2fad96161786f5c7e367694
Kristel Lamaj <kri.lamaj@gmail.com>
2021-07-08 14:08:17 +0300

    First Commit | Set Plug-in Information

---
+++ wp-cookie-bar.php
@@ -0,0 +1,11 @@
+<?php
+
+    /**
+     * Plugin Name: Wordpress Cookie Bar
+     * Description: This plug-in is used for cookies acception by the user.
+     * Version:     2.0
+     * Author:      PromoBeast Services
+     * Author URI:  https://icare.eu/
+    */
+
+?>
